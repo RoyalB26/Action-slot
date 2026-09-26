75a9d5cab94e8713a4b64d415227883ce6a91ae6
Bao Hoang <hgbao26@gmail.com>
2026-09-27 00:36:29 +0700

    update

--- datasets/taco.py
+++ datasets/taco.py
@@ -119,10 +119,10 @@ class TACO(Dataset):
                 video_folder = video_folder[0]
 
             parent_folder, basic, variant = scenario.split('/')
-            # folder_part= mapping[parent_folder]
-            # root= "/kaggle/input/datasets/royalb26/taco-dataset-" + folder_part + f"/Bn sao ca {parent_folder}"
-            # if folder_part == '2part':
-            #     root= "/kaggle/input/datasets/royalb26/taco-dataset-" + mapping[basic]
+            folder_part= mapping[parent_folder]
+            root= "/kaggle/input/datasets/royalb26/taco-dataset-" + folder_part + f"/Bn sao ca {parent_folder}"
+            if folder_part == '2part':
+                root= "/kaggle/input/datasets/royalb26/taco-dataset-" + mapping[basic]
             
             scenario_path = os.path.join(root,parent_folder,basic,'variant_scenario',variant)
             video_folder_path = os.path.join(scenario_path,'rgb',video_folder)
